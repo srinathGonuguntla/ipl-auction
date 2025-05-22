f8d454e0b021ee7fe0827f7c3b15a176745ec75d
srinath <gonuguntlasrinath@gmail.com>
2025-05-22 21:13:26 +0530

    update router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import './App.css';
+/* import './App.css';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { AuctionProvider } from './contexts/AuctionContext';
 import AppMain from './AppMain';
 
 function App() {
   return (
-     <AuctionProvider>
-      <AppMain />
-    </AuctionProvider>
+     <BrowserRouter basename="/ipl-auction">
+      <AuctionProvider>
+        <AppMain />
+      </AuctionProvider>
+    </BrowserRouter>
   );
 }
 
 export default App;
+ */
+
+
+import './App.css';
+import React from 'react';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { AuctionProvider } from './contexts/AuctionContext';
+import AppMain from './AppMain';
+
+function App() {
+  const isProduction = process.env.NODE_ENV === 'production';
+
+  if (isProduction) {
+    // For GitHub Pages deployment
+    return (
+      <BrowserRouter basename="/ipl-auction">
+        <AuctionProvider>
+          <AppMain />
+        </AuctionProvider>
+      </BrowserRouter>
+    );
+  } else {
+    // For local development
+    return (
+      <HashRouter>
+        <AuctionProvider>
+          <AppMain />
+        </AuctionProvider>
+      </HashRouter>
+    );
+  }
+}
+
+export default App;
+
