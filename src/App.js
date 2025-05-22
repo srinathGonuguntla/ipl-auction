43b8556acb437f50ce9787a1aa54847ba358c78d
srinath <gonuguntlasrinath@gmail.com>
2025-05-22 21:14:51 +0530

    update routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,3 @@
-/* import './App.css';
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { AuctionProvider } from './contexts/AuctionContext';
-import AppMain from './AppMain';
-
-function App() {
-  return (
-     <BrowserRouter basename="/ipl-auction">
-      <AuctionProvider>
-        <AppMain />
-      </AuctionProvider>
-    </BrowserRouter>
-  );
-}
-
-export default App;
- */
-
-
 import './App.css';
 import React from 'react';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
@@ -28,7 +8,7 @@ function App() {
   const isProduction = process.env.NODE_ENV === 'production';
 
   if (isProduction) {
-    // For GitHub Pages deployment
+   
     return (
       <BrowserRouter basename="/ipl-auction">
         <AuctionProvider>
@@ -37,7 +17,7 @@ function App() {
       </BrowserRouter>
     );
   } else {
-    // For local development
+    
     return (
       <HashRouter>
         <AuctionProvider>
